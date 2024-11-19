d63fc0cc7365a0b116620023a9354939bdb96041
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-11-19 10:43:12 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Assignments_3._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = {
+            { 2, 3, 4 },
+            { 1, 4, 6 }
+        };
+
+            PrintMatrix(matrix);
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write("| ");
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " | ");
+                }
+
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
